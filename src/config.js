0d685bab333480290b9241d90d7a054495e77d40
hanagejet <taroage@gmail.com>
2017-03-06 23:27:57 +0900

    Push data
    - Add html, createDate

---
+++ src/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  firebase: {
+    apiKey: "AIzaSyBByJTX-brI0MmkX8xf6mmoW8ABHKNQOOE",
+    authDomain: "notification-switch.firebaseapp.com",
+    databaseURL: "https://notification-switch.firebaseio.com",
+    storageBucket: "notification-switch.appspot.com",
+    messagingSenderId: "530170943747",
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const client = require('cheerio-httpcli');
+const config = require('./config.js');
+const firebase = require('firebase');
+const moment = require('moment');
 
-client.fetch('https://store.nintendo.co.jp/category/NINTENDOSWITCH/HAC_S_KAYAA.html')
+firebase.initializeApp(config.firebase);
+
+const fetch = client.fetch('https://store.nintendo.co.jp/category/NINTENDOSWITCH/HAC_S_KAYAA.html')
 .then(function(result) {
   const $ = result.$; // jQuery Objects
 
@@ -11,4 +16,18 @@ client.fetch('https://store.nintendo.co.jp/category/NINTENDOSWITCH/HAC_S_KAYAA.h
   console.log($('title').text());
 
   const buy_button = $('.content_area .add_area').html();
+  pushHtml(buy_button);
 })
+
+const pushHtml = function(html) {
+  firebase.database().ref('data').push({
+    html: html,
+    createDateAt: moment().format(),
+  })
+  .then(function(response) {
+    console.log('Success!!');
+  })
+  .catch(function(error) {
+    console.log(`Error Message: ${error}`);
+  });
+};
